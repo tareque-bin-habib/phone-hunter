c7c45bbcdbea0b1d966d4e628bfbee6b5e7226a1
tareque-bin-habib <shad.tbh@gmail.com>
2022-10-13 00:22:12 +0600

    initialize

---
+++ js/app.js
@@ -0,0 +1,80 @@
+const loadPhones = async (searchText, dataLimit) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhones(data.data, dataLimit)
+}
+
+const displayPhones = (phones, dataLimit) => {
+    const phoneContainer = document.getElementById('phones-container')
+    phoneContainer.textContent = '';
+    // display 10 phones only
+    const showAll = document.getElementById('show-all');
+    if (dataLimit && phones.length > 10) {
+        phones = phones.slice(0, 10);
+        showAll.classList.remove('d-none');
+    }
+    else {
+        showAll.classList.add('d-none');
+    }
+
+    // Display no phones found
+    const noPhone = document.getElementById('no found-msg');
+    if (phones.length === 0) {
+        noPhone.classList.remove('d-none');
+    }
+    else {
+        noPhone.classList.add('d-none')
+    }
+    // Display all Phones
+    phones.forEach(phone => {
+        console.log(phone)
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = `
+        <div class="card p-5">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        
+        `;
+        phoneContainer.appendChild(phoneDiv);
+    })
+    // stop Loader
+    toggleSpinner(false);
+
+}
+const processSearch = (dataLimit) => {
+    toggleSpinner(true);
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhones(searchText, dataLimit);
+}
+
+// Handle Search Button Click
+document.getElementById('btn-search').addEventListener('click', function () {
+    // start loader
+    processSearch(10);
+})
+
+// Spinner
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if (isLoading) {
+        loaderSection.classList.remove('d-none');
+    }
+    else {
+        loaderSection.classList.add('d-none');
+    }
+}
+
+// not the best way to show the All phones
+document.getElementById('btn-show-all').addEventListener('click', function () {
+    processSearch();
+})
+
+// loadPhones();
